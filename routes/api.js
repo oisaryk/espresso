3a9b7240ad9a4a65225ad372b23ef37715b30199
Nazar Yakymchuk <NazarYakymchuk@gmail.com>
2021-11-05 20:33:29 +0200

    [+] crypto

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const { success, error } = require('../utils/responses.js');
 const App  = require('../models/app.model');
+const axios = require('axios');
 
 /* Main API point */
 router.get('/', (req, res, next) => {
@@ -19,6 +20,41 @@ router.get('/', (req, res, next) => {
 		res.send(error('Error occurred with db interaction'));
 	})
 });
+
+router.get('/crypto', async (req, res) => {
+
+	 // axios.get('https://api2.binance.com/api/v3/ticker/24hr')
+		// .then(function (response) {
+		// 	// handle success
+		// 	let result = response.data
+	 //
+		// 	res.json('ok');
+		// })
+		// .catch(function (error) {
+		// 	// handle error
+		// 	console.log(error);
+		// 	res.end('no ok');
+		// })
+		// .finally(function () {
+		// 	res.end('ok')
+		// 	// always executed
+		// });
+
+	async function getCtypto() {
+		try {
+			const response = await axios.get('https://api2.binance.com/api/v3/ticker/24hr');
+			let result = response.data
+
+			res.json(result)
+			res.end()
+		} catch (error) {
+			res.end('error', error)
+		}
+	}
+
+	await getCtypto()
+})
+
 
 router.get('/records', async (req, res) => {
 	try {
